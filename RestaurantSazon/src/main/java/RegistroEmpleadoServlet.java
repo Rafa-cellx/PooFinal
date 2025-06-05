3c9315594a9085886690259a88c1bc6089b7355f
Rafa29 <crb2901@gmail.com>
2025-06-05 04:51:06 -0600

    volvio a funcionar el registro de empleados, se modifico el web.xml para incluir el RegistroEmpleadosServlet, se modifico RegistroEmpleado.html para responder al servlet y se corrigio RegistroEmpleadoServlet.java

--- RestaurantSazon/src/main/java/RegistroEmpleadoServlet.java
+++ RestaurantSazon/src/main/java/RegistroEmpleadoServlet.java
@@ -8,7 +8,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet("/registroEmpleado")
+@WebServlet("/RegistroEmpleadoServlet")
 public class RegistroEmpleadoServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
