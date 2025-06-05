1e0a29f4d23ce6a2949b080c71125a5e112d321b
Rafa29 <crb2901@gmail.com>
2025-06-05 04:25:26 -0600

    Se hizo el servlet de Registro de usuario

---
+++ RestaurantSazon/src/main/java/RegistroUsuarioServlet.java
@@ -0,0 +1,51 @@
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/RegistroUsuarioServlet")
+public class RegistroUsuarioServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String nombre = request.getParameter("nombre");
+        String apellido = request.getParameter("apellido");
+        String celular = request.getParameter("celular");
+        String correo = request.getParameter("correo");
+        String contrasena = request.getParameter("contrasena");
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection(
+
+                "jdbc:mysql://localhost:3306/sazon_db", "root", "MXVN#1champion5");
+
+
+
+
+            String sql = "INSERT INTO usuarios (nombre, apellido,celular,correo, contrasena) VALUES (?, ?, ?, ?,?)";
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setString(1, nombre);
+            ps.setString(2, apellido);
+            ps.setString(3, celular);
+            ps.setString(4, correo);
+            ps.setString(5, contrasena); 
+
+            ps.executeUpdate();
+            con.close();
+
+            response.setContentType("text/html;charset=UTF-8");
+            response.getWriter().println("<h1>Usuario registrado exitosamente</h1>");
+        } catch (Exception e) {
+            e.printStackTrace();
+            response.getWriter().println("<h1>Error: " + e.getMessage() + "</h1>");
+        }
+    }
+}
